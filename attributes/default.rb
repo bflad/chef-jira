ee98f9cba7713734366aad4bea2f475de75eb314
Brian Flad <bflad417@gmail.com>
2013-08-07 22:41:46 -0400

    Acutally bump default JIRA version to 6.0.6

--- attributes/default.rb
+++ attributes/default.rb
@@ -26,7 +26,7 @@ default['jira']['install_path']   = "/opt/atlassian/jira"
 default['jira']['install_type']   = "installer"
 default['jira']['url_base']       = "http://www.atlassian.com/software/jira/downloads/binary/atlassian-jira"
 default['jira']['user']           = "jira"
-default['jira']['version']        = "6.0.5"
+default['jira']['version']        = "6.0.6"
 
 if node['kernel']['machine'] == "x86_64"
   default['jira']['arch'] = "x64"
