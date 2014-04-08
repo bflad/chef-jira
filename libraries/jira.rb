30f6bc64ca929cb0fa8ca2e2671e6515099d967b
Brian Flad <bflad417@gmail.com>
2014-04-07 21:00:27 -0400

    Manual Rubocop fixes

--- libraries/jira.rb
+++ libraries/jira.rb
@@ -1,33 +1,39 @@
-# Chef::Recipe::Jira class
-class Chef::Recipe::Jira
-  def self.settings(node)
-    begin
-      if Chef::Config[:solo]
+# Chef class
+class Chef
+  # Chef::Recipe class
+  class Recipe
+    # Chef::Recipe::Jira class
+    class Jira
+      def self.settings(node)
         begin
-          settings = Chef::DataBagItem.load('jira', 'jira')['local']
-        rescue
-          Chef::Log.info('No jira data bag found')
-        end
-      else
-        begin
-          settings = Chef::EncryptedDataBagItem.load('jira', 'jira')[node.chef_environment]
-        rescue
-          Chef::Log.info('No jira encrypted data bag found')
-        end
-      end
-    ensure
-      settings ||= node['jira']
+          if Chef::Config[:solo]
+            begin
+              settings = Chef::DataBagItem.load('jira', 'jira')['local']
+            rescue
+              Chef::Log.info('No jira data bag found')
+            end
+          else
+            begin
+              settings = Chef::EncryptedDataBagItem.load('jira', 'jira')[node.chef_environment]
+            rescue
+              Chef::Log.info('No jira encrypted data bag found')
+            end
+          end
+        ensure
+          settings ||= node['jira']
 
-      case settings['database']['type']
-      when 'mysql'
-        settings['database']['port'] ||= 3306
-      when 'postgresql'
-        settings['database']['port'] ||= 5432
-      else
-        Chef::Log.warn('Unsupported database type.')
+          case settings['database']['type']
+          when 'mysql'
+            settings['database']['port'] ||= 3306
+          when 'postgresql'
+            settings['database']['port'] ||= 5432
+          else
+            Chef::Log.warn('Unsupported database type.')
+          end
+        end
+
+        settings
       end
     end
-
-    settings
   end
 end
