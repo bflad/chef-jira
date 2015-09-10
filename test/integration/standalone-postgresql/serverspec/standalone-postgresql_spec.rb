8624fe5fea807e53f48f19543233bf26947db97e
Gavin Reynolds <g.reynolds@src.gla.ac.uk>
2015-09-10 12:40:06 +0100

    Add additional suite, standalone-postgresql. Add serverspec, remove minitest.

---
+++ test/integration/standalone-postgresql/serverspec/standalone-postgresql_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe 'Java' do
+  describe command('java -version 2>&1') do
+    its(:exit_status) { should eq 0 }
+  end
+end
+
+describe 'Postgresql' do
+  describe port(5432) do
+    it { should be_listening }
+  end
+end
+
+describe 'JIRA' do
+  describe port(8080) do
+    it { should be_listening }
+  end
+
+  describe command("curl --noproxy localhost 'http://localhost:8080/secure/SetupApplicationProperties!default.jspa' | grep 'JIRA Setup'") do
+    its(:exit_status) { should eq 0 }
+  end
+end
+
+describe 'Apache2' do
+  describe port(80) do
+    it { should be_listening }
+  end
+
+  describe port(443) do
+    it { should be_listening }
+  end
+
+  describe command("curl --insecure --noproxy localhost 'https://localhost/secure/SetupApplicationProperties!default.jspa' | grep 'JIRA Setup'") do
+    its(:exit_status) { should eq 0 }
+  end
+end
